20e0b8aefe1fb50baaaca251f208077cdb257f29
Dmytro Skrypchenko <Dmytro_Skrypchenko@epam.com>
2017-04-14 18:06:36 +0300

    implement add product methods

--- Stock.WebServices.Product/IProductService.cs
+++ Stock.WebServices.Product/IProductService.cs
@@ -1,11 +1,25 @@
 ﻿namespace Stock.WebServices.Product
 {
     using System.ServiceModel;
+    using System.Collections.Generic;
+    using BL.DtoEntities;
 
     [ServiceContract]
     public interface IProductService
     {
         [OperationContract]
         string GetData(int value);
+
+        [OperationContract]
+        void AddPhone(PhoneDto phone);
+
+        [OperationContract]
+        void AddElectronicBook(ElectronicBookDto electronicBook);
+
+        [OperationContract]
+        void AddSmartWatch(SmartWatchDto smartWatch);
+
+        [OperationContract]
+        IEnumerable<ProductDto> GetAllProducts();
     }
 }
--- Stock.WebServices.Product/ProductService.cs
+++ Stock.WebServices.Product/ProductService.cs
@@ -1,10 +1,55 @@
 ﻿namespace Stock.WebServices.Product
 {
+    using System.Collections.Generic;
+    using BL.DtoEntities;
+    using BL.Services.Abstract;
+    using BL.Services.Concrete;
+
     public class ProductService : IProductService
     {
+        private readonly IElectronicBookService _electronicBookService;
+        private readonly ISmartWatchService _smartWatchService;
+        private readonly IPhoneService _phoneService;
+
+        public ProductService()
+        {
+            _smartWatchService = new SmartWatchService();
+            _electronicBookService = new ElectronicBookService();
+            _phoneService = new PhoneService();
+        }
+
         public string GetData(int value)
         {
             return $"You entered: {value}";
+        }
+
+        public void AddPhone(PhoneDto phone)
+        {
+            _phoneService.Insert(phone);
+        }
+
+        public void AddElectronicBook(ElectronicBookDto electronicBook)
+        {
+            _electronicBookService.Insert(electronicBook);
+        }
+
+        public void AddSmartWatch(SmartWatchDto smartWatch)
+        {
+            _smartWatchService.Insert(smartWatch);
+        }
+
+        public IEnumerable<ProductDto> GetAllProducts()
+        {
+            var phones = _phoneService.GetAll();
+            var electronicBooks = _electronicBookService.GetAll();
+            var smartWatches = _smartWatchService.GetAll();
+
+            var productsDto = new List<ProductDto>();
+            productsDto.AddRange(phones);
+            productsDto.AddRange(electronicBooks);
+            productsDto.AddRange(smartWatches);
+
+            return productsDto;
         }
     }
 }
